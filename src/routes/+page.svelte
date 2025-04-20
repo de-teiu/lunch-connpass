b85bae286c8c81f767626bb8a54df51d2eb8c130
DE-TEIU <tekuragari@gmail.com>
2025-04-20 21:20:36 +0900

    検索ボタンホバー時にカーソルが変わるように修正

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -87,7 +87,7 @@
       <div>
         <button
           type="submit"
-          class="w-full rounded-md bg-blue-600 px-4 py-2 font-medium text-white hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none md:w-auto"
+          class="w-full rounded-md bg-blue-600 px-4 py-2 font-medium text-white hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none md:w-auto hover:cursor-pointer"
           disabled={isLoading}
         >
           検索
